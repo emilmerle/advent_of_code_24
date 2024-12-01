4fd49d75191dbe203b384dc52c097ff57a242eba
Emil Merle <emilmerle8@gmail.com>
2024-12-01 21:21:58 +0100

    Added template for other days in python

---
+++ template/main.py
@@ -0,0 +1,58 @@
+from pathlib import Path
+
+# Code Snippet for reading a file:
+"""
+path = Path(input_filename)
+with open(path, "r") as f:
+"""
+
+
+def algorithm_1(input_filename: str) -> int:
+    answer = 1
+    return answer
+
+
+def algorithm_2(input_filename: str) -> int:
+    answer = 2
+    return answer
+
+
+# Testing and solving functions
+def test_part1() -> bool:
+    filename = "./input_files/test.txt"
+    expected_answer = 11
+    algorithm_answer = algorithm_1(filename)
+    return expected_answer == algorithm_answer
+
+
+def test_part2() -> bool:
+    filename = "./input_files/test.txt"
+    expected_answer = 31
+    algorithm_answer = algorithm_2(filename)
+    return expected_answer == algorithm_answer
+
+
+def solve_part1() -> int:
+    filename = "./input_files/puzzle.txt"
+    algorithm_answer = algorithm_1(filename)
+    return algorithm_answer
+
+
+def solve_part2() -> int:
+    filename = "./input_files/puzzle.txt"
+    algorithm_answer = algorithm_2(filename)
+    return algorithm_answer
+
+
+if __name__ == "__main__":
+    if test_part1():
+        answer = solve_part1()
+        print(f"Todays answer of part 1 is {answer}")
+    else:
+        print("Test of part 1 was not successfull")
+
+    if test_part2():
+        answer = solve_part2()
+        print(f"Todays answer of part 2 is {answer}")
+    else:
+        print("Test of part 2 was not successfull")
