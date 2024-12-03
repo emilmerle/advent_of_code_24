b505fc7b7932075af984cce69ca370e73fd3ff61
Emil Merle <emilmerle8@gmail.com>
2024-12-03 10:50:44 +0100

    Solution for day03 part 1

---
+++ test.py
@@ -0,0 +1,6 @@
+"""
+To see if a list only has some given elements:
+Convert list to a set and compare to the given set:
+set(list_to_test) <= {1, 2, 3}
+This gives True if the list only has 1, 2 or 3 in it.
+"""
