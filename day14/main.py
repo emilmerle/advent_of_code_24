d869c5b160f1bf26058b81b26a6a441c76e52d97
Emil Merle <emilmerle8@gmail.com>
2025-01-09 13:24:52 +0100

    Solution for day14 part 1

---
+++ day14/main.py
@@ -0,0 +1,107 @@
+import math
+from pathlib import Path
+import re
+
+"""
+Idea Part 1:
+
+"""
+
+
+def algorithm_1(input_filename: str, is_test: bool) -> int:
+    path = Path(input_filename)
+    time = 100
+    dim_x, dim_y = 101, 103
+    if is_test:
+        dim_x, dim_y = 11, 7
+    position_pattern = r"(\d{1,3}),(\d{1,3})"
+    velocity_pattern = r"(-?\d{1,3}),(-?\d{1,3})"
+    quadrant_counter = [0,0,0,0]
+    with open(path, "r") as file:
+        for line in file:
+            position_string, velocity_string = line.split(" ")[0], line.split(" ")[1]
+            position_matches = re.findall(position_pattern, position_string)
+            position = int(position_matches[0][0]), int(position_matches[0][1])
+            velocity_matches = re.findall(velocity_pattern, velocity_string)
+            velocity = int(velocity_matches[0][0]), int(velocity_matches[0][1])
+
+            final_pos_x = (position[0] + time * velocity[0]) % dim_x
+            final_pos_y = (position[1] + time * velocity[1]) % dim_y
+            
+            cutoff_x = dim_x // 2
+            cutoff_y = dim_y // 2
+            
+            if final_pos_x < cutoff_x:
+                if final_pos_y < cutoff_y:
+                    quadrant_counter[0] += 1
+                elif final_pos_y > cutoff_y:
+                    quadrant_counter[1] += 1
+            elif final_pos_x > cutoff_x:
+                if final_pos_y < cutoff_y:
+                    quadrant_counter[2] += 1
+                elif final_pos_y > cutoff_y:
+                    quadrant_counter[3] += 1
+
+    total_valid_robots = math.prod(quadrant_counter)
+    return total_valid_robots
+            
+            
+
+
+
+
+
+"""
+Idea Part 2:
+
+"""
+
+
+def algorithm_2(input_filename: str, is_test: bool) -> int:
+    path = Path(input_filename)
+    with open(path, "r") as file:
+        pass
+
+
+# Helping functions
+
+
+# Testing and solving functions
+def test_part1() -> bool:
+    filename = "./input_files/test.txt"
+    expected_answer = 12
+    algorithm_answer = algorithm_1(filename, True)
+    return expected_answer == algorithm_answer
+
+
+def test_part2() -> bool:
+    filename = "./input_files/test.txt"
+    expected_answer = 31
+    algorithm_answer = algorithm_2(filename, True)
+    return expected_answer == algorithm_answer
+
+
+def solve_part1() -> int:
+    filename = "./input_files/puzzle.txt"
+    algorithm_answer = algorithm_1(filename, False)
+    return algorithm_answer
+
+
+def solve_part2() -> int:
+    filename = "./input_files/puzzle.txt"
+    algorithm_answer = algorithm_2(filename, False)
+    return algorithm_answer
+
+
+if __name__ == "__main__":
+    if test_part1():
+        answer = solve_part1()
+        print(f"Todays answer of part 1 is {answer}")
+    else:
+        print("Test of part 1 was not successfull")
+
+    if test_part2():
+        answer = solve_part2()
+        print(f"Todays answer of part 2 is {answer}")
+    else:
+        print("Test of part 2 was not successfull")
